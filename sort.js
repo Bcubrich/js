33431d2fbdcc6c186d6bc4806168a508c404a8e9
Bart Cubrich <41648759+Bcubrich@users.noreply.github.com>
2020-12-22 14:59:07 -0700

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,13 +1,16 @@
-const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+var getCellValue = function(tr, idx){ return tr.children[idx].innerText || tr.children[idx].textContent; }
 
-const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+var comparer = function(idx, asc) { return function(a, b) { return function(v1, v2) {
+        return v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2);
+    }(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+}};
 
 // do the work...
-document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-    const table = th.closest('table');
-    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-        .forEach(tr => table.appendChild(tr) );
-})));
+Array.prototype.slice.call(document.querySelectorAll('th')).forEach(function(th) { th.addEventListener('click', function() {
+        var table = th.parentNode
+        while(table.tagName.toUpperCase() != 'TABLE') table = table.parentNode;
+        Array.prototype.slice.call(table.querySelectorAll('tr:nth-child(n+2)'))
+            .sort(comparer(Array.prototype.slice.call(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+            .forEach(function(tr) { table.appendChild(tr) });
+    })
+});
